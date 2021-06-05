1614668ccd603eabf372262fe86342a0fb8a2224
SheillyR <sheillyrolo@gmail.com>
2021-06-04 19:02:11 -0500

    implementacion de tests

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -16,6 +16,7 @@ public class StudentService {
     private final StudentRepository studentRepository;
 
     public List<Student> getAllStudents() {
+
         return studentRepository.findAll();
     }
 
